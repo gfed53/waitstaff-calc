2f27f1c841dd156daa1530d876bf0523ba771176
gfed53 <gfederico7153@gmail.com>
2016-02-01 17:48:21 -0500

    Initialized myCtrl, variables and submit method

--- apps/app.js
+++ apps/app.js
@@ -1,1 +1,16 @@
 angular.module('myApp', [])
+.controller('myCtrl', function($scope){
+	$scope.baseMealPrice = '';
+	$scope.taxRate = '';
+	$scope.tipPercent = '';
+	$scope.subTotal = '';
+	$scope.tip = '';
+	$scope.total = '';
+	$scope.tipTotal = '';
+
+
+	$scope.submit = function(e){
+		e.preventDefault();
+		console.log($scope.baseMealPrice);
+	};
+});
