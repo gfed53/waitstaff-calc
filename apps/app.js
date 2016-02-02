b415a6948c5267357e69fe317a10e9e97fae7e25
gfed53 <gfederico7153@gmail.com>
2016-02-02 16:17:16 -0500

    Submit button takes data from meal details, plugs into customer charges

--- apps/app.js
+++ apps/app.js
@@ -1,16 +1,22 @@
-angular.module('myApp', [])
+angular.module('myApp', ['ngMessages'])
 .controller('myCtrl', function($scope){
 	$scope.baseMealPrice = '';
 	$scope.taxRate = '';
 	$scope.tipPercent = '';
-	$scope.subTotal = '';
-	$scope.tip = '';
-	$scope.total = '';
-	$scope.tipTotal = '';
+	$scope.subTotal = 0;
+	$scope.tip = 0;
+	$scope.total = 0;
+	$scope.tipTotal = 0;
+	$scope.mealCount = 0;
+	$scope.avgTip = 0;
 
 
-	$scope.submit = function(e){
-		e.preventDefault();
+	$scope.submit = function(){
 		console.log($scope.baseMealPrice);
+		var mealTax = $scope.baseMealPrice * $scope.taxRate;
+		$scope.subTotal = ($scope.baseMealPrice+mealTax);
+		var mealTip = $scope.subTotal * $scope.tipPercent;
+		$scope.tip = mealTip;
+		$scope.total = ($scope.subTotal+mealTip);
 	};
 });
