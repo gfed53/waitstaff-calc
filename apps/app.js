a32e23e1230726384d6c471ec9bb90dc9ddfa2ab
gfed53 <gfederico7153@gmail.com>
2016-02-02 16:44:21 -0500

    Earnings info updates, comments added, number filters added

--- apps/app.js
+++ apps/app.js
@@ -12,11 +12,20 @@ angular.module('myApp', ['ngMessages'])
 
 
 	$scope.submit = function(){
-		console.log($scope.baseMealPrice);
-		var mealTax = $scope.baseMealPrice * $scope.taxRate;
-		$scope.subTotal = ($scope.baseMealPrice+mealTax);
-		var mealTip = $scope.subTotal * $scope.tipPercent;
-		$scope.tip = mealTip;
-		$scope.total = ($scope.subTotal+mealTip);
+		if($scope.myForm.$valid) {
+			//Combine base meal price and tax to get sub total.
+			var mealTax = $scope.baseMealPrice * $scope.taxRate;
+			$scope.subTotal = ($scope.baseMealPrice+mealTax);
+			//Calculate tip from tip percentage and add to subtotal.
+			var mealTip = $scope.subTotal * $scope.tipPercent;
+			$scope.tip = mealTip;
+			$scope.total = ($scope.subTotal+mealTip);
+			//Add the meal's tip to tip total.
+			$scope.tipTotal += mealTip;
+			//Increase the meal count
+			$scope.mealCount += 1;
+			//Calculate average tip per meal.
+			$scope.avgTip = $scope.tipTotal / $scope.mealCount;
+		}								
 	};
 });
